222a93f83566087ad61971d15c7997bbf7b1cfc8
Chris Pillion <christopherpillion@gmail.com>
2022-02-18 13:16:26 -0800

    Added interfaces for updating colors

--- sample/client/src/UIToolbar.jsx
+++ sample/client/src/UIToolbar.jsx
@@ -1,57 +1,82 @@
-import React from "react";
-import scUpdate from "./sc-update-client";
+import React from 'react';
+import Communicator from "communicator";
+import scUpdate from './sc-update-client';
 
 export default class UIToolbar extends React.Component {
+  constructor() {
+    super();
+    this.scUpdate = new scUpdate('http://localhost:5000');
+  }
 
-    constructor() {
-        super();
-        this.scUpdate = new scUpdate("http://localhost:5000");
+  addNodeProperties() {
+    let hwv = this.props.viewer;
+    let selectionResults = hwv.selectionManager.getResults();
+    // Start with one selection. TODO: add support from multiple selections
+    let nodeId;
+    if (selectionResults.length === 0) {
+      alert('Please first select the nodes you would like to add properties for');
+    } else {
+      // Todo: Create modal, form, etc to collect this data. Using prompt for now.
+      let propertyName = prompt('Enter the property name: ', 'Material');
+      let propertyValue = prompt(`Enter the value for the ${propertyName} property: `, '');
+      nodeId = selectionResults[0].getNodeId();
+      // Use SC Update functions to send this data to the client - will need selected nodeID as well
+      this.scUpdate.updateAttributes(nodeId, { [propertyName]: propertyValue });
+      this.scUpdate.sendToLibSc();
     }
+  }
 
-    addNodeProperties() {
-        let hwv = this.props.viewer;
-        let selectionResults = hwv.selectionManager.getResults();
-        // Start with one selection. TODO: add support from multiple selections
-        let nodeId;
-        if (selectionResults.length === 0) {
-            alert("Please first select the nodes you would like to add properties for");
-        }
-        else {
-            // Todo: Create modal, form, etc to collect this data. Using prompt for now. 
-            let propertyName = prompt("Enter the property name: ", "Material");
-            let propertyValue = prompt(`Enter the value for the ${propertyName} property: `, "");
-            nodeId = selectionResults[0].getNodeId;
-            // Use SC Update functions to send this data to the client - will need selected nodeID as well
-            this.scUpdate.updateAttributes(nodeId, {[propertyName]:propertyValue});
-            this.scUpdate.sendToLibSc();
-        }
-    }
+  updateDefaultView() {
+    let camera = this.props.viewer.view.getCamera();
+    this.scUpdate.updateDefaultCameraView(camera);
+    this.scUpdate.sendToLibSc();
+  }
 
-    updateDefaultView() {
-        let camera = this.props.viewer.view.getCamera();
-        this.scUpdate.updateDefaultCameraView(camera);
-        this.scUpdate.sendToLibSc();
-    }
+  updateModelColors() {
+    let nodeIds = [8,9,10,11]; // Screws nodeIds
+    let color = new Communicator.Color.red();
+    this.scUpdate.updateColors(nodeIds, color);
+    this.scUpdate.sendToLibSc();
+  }
 
-
-    render() {
-        return(
-            <>
-                <button id="home-button" onClick={() => {
-                    this.props.viewer.view.resetCamera(750)
-                    .then( () => this.props.viewer.view.fitWorld(750) )
-                }
-                }>Home</button>
-                <button id="add-property-button" onClick={() => {
-                    this.addNodeProperties();
-                }
-                }>Add Node Property</button>
-                <button id="update-view-button" onClick={() => {
-                    this.updateDefaultView();
-                }
-                }>Update Default View</button>
-            </>
-        )
-    }
-
+  render() {
+    return (
+      <>
+        <button
+          id="home-button"
+          onClick={() => {
+            this.props.viewer.view
+              .resetCamera(750)
+              .then(() => this.props.viewer.view.fitWorld(750));
+          }}
+        >
+          Home
+        </button>
+        <button
+          id="add-property-button"
+          onClick={() => {
+            this.addNodeProperties();
+          }}
+        >
+          Add Node Property
+        </button>
+        <button
+          id="update-view-button"
+          onClick={() => {
+            this.updateDefaultView();
+          }}
+        >
+          Update Default View
+        </button>
+        <button
+          id="update-colors-button"
+          onClick={() => {
+            this.updateModelColors();
+          }}
+        >
+          Save Assigned Colors
+        </button>
+      </>
+    );
+  }
 }
--- sample/client/src/sc-update-client.js
+++ sample/client/src/sc-update-client.js
@@ -1,33 +1,40 @@
-import io from "socket.io-client";
+import io from 'socket.io-client';
 
 export default class scUpdate {
+  constructor(libScServerEndpoint) {
+    this.scChanges = { attributes: [], defaultCamera: null, colors: [] };
+    this.socket = io(libScServerEndpoint, { transports: ['websocket'] });
 
-    constructor(libScServerEndpoint) {
-        this.scChanges = {attributes: [], defaultCamera: null};
-        this.socket = io(libScServerEndpoint, { transports: ["websocket"] });
+    // Socket.io error info for debugging
+    this.socket.on('connect_error', (err) => {
+      console.log(`connect_error due to ${err.message}`);
+    });
+  }
 
-        // Socket.io error info for debugging
-        this.socket.on("connect_error", (err) => {
-            console.log(`connect_error due to ${err.message}`);
-        });
-    }
+  updateAttributes(nodeId, nodeAttributes) {
+    //Need to add check if nodeId already has attribute updates associated
+    let nodeInfo = {
+      "nodeId": nodeId,
+    };
+    let updateInfo = Object.assign(nodeInfo, nodeAttributes);
+    this.scChanges.attributes.push(updateInfo);
+  }
 
-    updateAttributes(nodeId, nodeAttributes) {
-        //Need to add check if nodeId already has attribute updates associated
-        let nodeInfo = {
-            "nodeId" : nodeId,
-        }
-        let updateInfo = Object.assign(nodeInfo, nodeAttributes)
-        this.scChanges.attributes.push(updateInfo);
-    }
+  updateDefaultCameraView(camera) {
+    this.scChanges.defaultCamera = camera;
+  }
 
-    updateDefaultCameraView(camera) {
-        this.scChanges.defaultCamera = camera;
-    }
+  // Will need to update this for using something like a color map in HC
+  updateColors(nodeIds, color) {
+    let updateInfo = {
+      "nodeIds": nodeIds,
+      "color": color.toJson()
+    };
+    this.scChanges.colors.push(updateInfo);
+  }
 
-    sendToLibSc() {
-        this.socket.emit('sc_update_to_author', JSON.stringify(this.scChanges));
-        // Add method to clear tracked updates once they are successfully published. 
-    }
-
+  sendToLibSc() {
+    this.socket.emit('sc_update_to_author', JSON.stringify(this.scChanges));
+    // Add method to clear tracked updates once they are successfully published.
+  }
 }
